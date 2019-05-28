7c64dc131b6c488f7d4f6312bed44f9a3fedc6ad
evanthebouncy <evanthebouncy@gmail.com>
2019-05-28 17:28:05 -0400

    some bug getting stuck ?!

--- maze.py
+++ maze.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 # the neighbor function of up/down/left/right moves
-def get_neib_fun(W,H,maze):
+def get_neib_fun(maze):
+    H,W = maze.shape
     def neib(x,y):
         ret = []
         for cand in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]:
@@ -18,7 +21,7 @@ def get_neib_fun(W,H,maze):
 
 def make_path(maze):
     H,W = maze.shape
-    neib = get_neib_fun(W,H,maze)
+    neib = get_neib_fun(maze)
     def get_neib_vals(neibs, dists):
         return [dists[yy,xx] for xx,yy in neibs] + [np.inf]
 
@@ -60,10 +63,51 @@ def make_valid_maze(W,H,density):
     else:
         return make_valid_maze(W,H,density)
 
+def draw_maze(maze):
+    plt.imshow(maze)
+    plt.colorbar()
+    plt.savefig('drawings/maze_path.png')
+
+def get_1_move(maze, dists, particles, particles_move_mask):
+    neib_fun = get_neib_fun(maze)
+    for x,y in particles:
+        assert dists[y][x] != np.inf, "oy ur standing on impossible blocks"
+
+    particles = [_ for _ in particles]
+
+    for i,p in enumerate(particles):
+        if particles_move_mask[i] is False:
+            continue
+        p_neib = neib_fun(*p)
+        random.shuffle(p_neib)
+        for p_n in p_neib:
+            if p_n not in particles:
+                cur_dist = dists[p[1]][p[0]]
+                new_dist = dists[p_n[1]][p_n[0]]
+                if new_dist < cur_dist:
+                    particles[i] = p_n
+                    return True, i, particles
+    return False, None, particles
+
+def get_group_move(maze, dists, particles):
+    move_mask = [True for _ in range(len(particles))]
+
+    while True:
+        can_move, moved_id, particles = get_1_move(maze, dists, particles, move_mask)
+        if can_move is False:
+            return particles
+        else:
+            move_mask[moved_id] = False
+
+
 if __name__ == '__main__':
     maze,dists = make_valid_maze(10, 16, 0.2)
     print (maze)
     print (dists)
+    draw_maze(dists)
 
-
+    particles = [(i,0) for i in range(10)]
+    for i in range(100):
+        print (particles)
+        particles = get_group_move(maze, dists, particles)
 
